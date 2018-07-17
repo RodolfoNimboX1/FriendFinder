d4b0f7ab299709b3c1d46ccc8724c4b86392e0ed
Rodolfo Resines <rodolfo.sresines@gmail.com>
2018-07-16 19:50:22 -0500

    made changes to the routing folder, created the data array for the responses and created the survey with the js logic

---
+++ app/data/friends.js
@@ -0,0 +1,21 @@
+// DATA
+// ===============================================================================
+var surveyArray = [
+    {
+        name: "Rodolfo Schaefer Resines",
+        photo: "https://scontent.fntr3-1.fna.fbcdn.net/v/t1.0-9/23755752_10155684305791285_5675570171571462083_n.jpg?_nc_cat=0&oh=0351cebb4e3a2951158b449755e32d21&oe=5BCDEF73",
+        scores: [
+            4,
+            4,
+            4,
+            3,
+            4,
+            4,
+            4,
+            4,
+            2,
+            5
+        ]
+    }
+];
+module.exports = surveyArray
--- routing/apiRoutes.js
+++ routing/apiRoutes.js
@@ -1,11 +1,11 @@
 // ROUTES
 // ==============================================================================
-var path = require("path");
+var friends = require(".../data/friends");
 
 module.exports = function(app) {
 // GET route with the url /api/friends. This will be used to display a JSON of all possible friends.
 app.get("/api/friends", function(req, response) {
-   // pending logic
+   response.json(friends);
 });
 
 // POST route /api/friends. This will be used to handle incoming survey results. This route will also be used to handle the compatibility logic.
@@ -13,4 +13,4 @@ app.get("/api/friends", function(req, response) {
     // pending logic
 });
 
-}
+};
