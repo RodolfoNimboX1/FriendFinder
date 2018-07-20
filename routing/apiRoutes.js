7238c4e4fadfb06508585f8387bbde2f478c9e47
Rodolfo Resines <rodolfo.sresines@gmail.com>
2018-07-20 16:24:07 -0500

    finished the logic for selectiong the best match

--- routing/apiRoutes.js
+++ routing/apiRoutes.js
@@ -11,7 +11,53 @@ app.get("/api/friends", function(req, response) {
 // POST route /api/friends. This will be used to handle incoming survey results. This route will also be used to handle the compatibility logic.
 app.post("/api/friends", function(req, response) {
     console.log(req.body);
-    friends.push(req.body);
+    //friends.push(req.body);
+    // newGamer is going to be the new user that filled the form
+    var newGamer = req.body;
+
+    // looking for best match possible
+    var match = {};
+
+    for (var i = 0; i < newGamer.scores.length; i++) {
+        if (newGamer.scores[i] == "1 (Strongly Disagree)") {
+            newGamer.scores[i] = 1;
+        } // if ends 
+        else if (newGamer.scores[i] == "5 (Strongly Agree)") {
+            newGamer.scores[i] == 5;
+        } // else if ends
+        else {
+            newGamer.scores[i] = parseInt(newGamer.scores[i]);
+        } // else ends
+    } // for ends
+
+    // compare scores from newGamer to api friend list
+
+    var bestMatch = 0;
+    //greatest score difference for a question is 4, therefore greatest difference is 4 times # of questions in survey
+    var matchDifference = 40;
+
+    for (var i = 0; i < friends.length; i++) {
+        var totalDiff = 0;
+
+        for (var j = 0; j < friends[i].scores.length; j++) {
+            var diffOnScore = Math.abs(friends[i].scores[j] - newGamer.scores[j]);
+            totalDiff += diffOnScore;
+        }// second for ends
+
+        // if totalDiff in scores is less than the best match so far
+        // save that index and difference
+        if (totalDiff < matchDifference) {
+            bestMatch = i;
+            matchDifference = totalDiff;
+        } // if ends
+
+    }// first for ends
+
+    match = friends[bestMatch];
+
+    friends.push(newGamer);
+
+    response.json(match);
 });
 
 }
